5abb0ee15d5624bb60b6be20bda1f521377e85c6
Xuân Đại <daik5xuanvinh@gmail.com>
2026-04-21 22:25:44 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final float PI = 3.14f;
 
-        System.out.print("bán kính : ");
-        float k1 = sc.nextFloat();
-        System.out.printf("Diện tích : %.2f", PI * k1 * k1);
+        System.out.print("widt = ");
+        float w = sc.nextFloat();
+        System.out.print("height = ");
+        float h = sc.nextFloat();
+
+        System.out.printf("Diện tích : %.2f\n", w * h);
+        System.out.printf("Chu vi : %.2f", 2 * (w + h));
     }
 }
